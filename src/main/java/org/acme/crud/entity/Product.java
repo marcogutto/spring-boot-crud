df5ab0eac1863e9dcc54089284e18005c97bf0c8
Marco Seabra <marcogutto@brq.com>
2023-05-25 09:04:15 -0300

    Ajuste table generator ...

--- src/main/java/org/acme/crud/entity/Product.java
+++ src/main/java/org/acme/crud/entity/Product.java
@@ -6,18 +6,21 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "PRODUCT_TBL")
+@Table(name = "product_tbl")
 public class Product {
 
     @Id
-    @GeneratedValue
+    @TableGenerator(name="product_seq", table="sequence", initialValue = 0, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator="product_seq")
     private int id;
     private String name;
     private int quantity;
